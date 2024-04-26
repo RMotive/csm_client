547e28a2a893e926a5034cbfdc5c83e67d016efb
Renato Urrea <usuaryrenato@hotmail.com>
2024-04-25 23:18:21 -0700

    conditional export

--- lib/src/common/tools/common_tools_module.dart
+++ lib/src/common/tools/common_tools_module.dart
@@ -1,1 +1,2 @@
-export 'csm_deserealizer.dart';
+export 'csm_deserealizer_native.dart' if (dart.library.html) "csm_deserealize_web.dart";
+
---
+++ lib/src/common/tools/csm_deserealize_web.dart
@@ -0,0 +1,5 @@
+import 'package:csm_foundation_services/csm_foundation_services.dart';
+
+T deserealize<T>(JObject object) {
+  throw UnimplementedError();
+}
