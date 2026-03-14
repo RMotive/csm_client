a46d240748e0c22cd60e3398ba060a2d36834d79
jr110 <c.d@hotmail.es>
2026-03-13 18:21:25 -0700

    testing package ref change

--- csm_client_testing/lib/src/core/utils/testing_client_utils.dart
+++ csm_client_testing/lib/src/core/utils/testing_client_utils.dart
@@ -8,7 +8,7 @@ final class TestingClientUtils {
   /// Creates a [MockClient] instance object for {definition} purposes tests, overriding the actual [Client] that makes network request to the actual server.
   ///
   /// [endpointMocks] a [Map] object that stores the endpoint [String] identification and the overriden [T] object to force as a response inside a [SuccessFrame].
-  static MockClient createMockClient<T extends EncodableI>(Map<String, T> endpointMocks) {
+  static MockClient createMockClient<T extends IEncodable>(Map<String, T> endpointMocks) {
     final MockClient mockClient = MockClient(
       (Request request) async {
         final String endpointSegment = request.url.pathSegments.last;
